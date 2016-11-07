ec630a84e015327ccd2b9bbb4ef398ab808d9fd3
emaLorenzo <emalorenzo2011@gmail.com>
2016-11-07 18:27:54 -0300

    Fix navbar

--- db/schema.rb
+++ db/schema.rb
@@ -31,14 +31,6 @@ ActiveRecord::Schema.define(version: 20161103184943) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "min_point"
-    t.integer  "max_point"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "cities", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -56,10 +48,11 @@ ActiveRecord::Schema.define(version: 20161103184943) do
     t.integer  "id_user"
     t.integer  "id_city"
     t.string   "title"
+    t.string   "photo_url",   default: "logo.png"
     t.text     "description"
-    t.boolean  "done"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.boolean  "done",        default: false
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
   end
 
   create_table "postulations", force: :cascade do |t|
@@ -81,20 +74,20 @@ ActiveRecord::Schema.define(version: 20161103184943) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.integer  "id_achievement"
+    t.integer  "id_achievement",         default: 0
     t.string   "name"
     t.integer  "dni"
     t.date     "birthdate"
     t.string   "phone"
-    t.boolean  "is_admin"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.boolean  "is_admin",               default: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
