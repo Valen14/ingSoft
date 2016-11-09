eb32f5d11af7bb92baf43aa1eeed48f91d1f258d
dibene santiago <dibenesantiago@gmail.com>
2016-11-09 12:24:17 -0300

    fixiduser

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,9 +22,9 @@ Post.create(id:"8" ,user_id: "3" ,title:"Busco acompañante de viaje3" ,city_id:
 
 
 puts "add User"
-User.create(id:"1", name:"usuario_test1" ,dni:"123456", birthdate:"01/01/2016", phone:"1213456" ,is_admin:"false" ,email:"usuario_test1@test.com", password:"usuario_test1" )
-User.create(id:"2", name:"usuario_test2" ,dni:"123456", birthdate:"01/01/2016", phone:"1213456" ,is_admin:"false" ,email:"usuario_test2@test.com", password:"usuario_test2" )
-User.create(id:"3", name:"usuario_test3" ,dni:"123456", birthdate:"01/01/2016", phone:"1213456" ,is_admin:"false" ,email:"usuario_test3@test.com", password:"usuario_test3" )
+User.create(id:"11", name:"usuario_test1" ,dni:"123456", birthdate:"01/01/2016", phone:"1213456" ,is_admin:"false" ,email:"usuario_test1@test.com", password:"usuario_test1" )
+User.create(id:"12", name:"usuario_test2" ,dni:"123456", birthdate:"01/01/2016", phone:"1213456" ,is_admin:"false" ,email:"usuario_test2@test.com", password:"usuario_test2" )
+User.create(id:"13", name:"usuario_test3" ,dni:"123456", birthdate:"01/01/2016", phone:"1213456" ,is_admin:"false" ,email:"usuario_test3@test.com", password:"usuario_test3" )
 
 User.create(id:"0", name:"admin_test" ,dni:"123456", birthdate:"01/01/2016", phone:"1213456" ,is_admin:"true" ,email:"admin_test@test.com", password:"admin_test" )
 puts "add City"
